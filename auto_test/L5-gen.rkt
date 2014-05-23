da9b91941d47d56caa4b6cafcfbb5f55fbd83646
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-05-23 01:22:30 -0500

    start lambda support

--- auto_test/L5-gen.rkt
+++ auto_test/L5-gen.rkt
@@ -3,7 +3,20 @@
 (define (gen-random-L5 depth)
   (random-L5 depth '()))
 
+(define arg-vars '(x y z t q))
+
 (define (random-L5 depth vars)
+  
+  (define (random-lambda d bindings)
+    (define random-arglist
+      (λ (len)
+        (if (= len 0)
+            '()
+            (cons (list-ref arg-vars len)
+                  (random-arglist (- len 1))))))
+    (let [(lambda-args (random-arglist (random 4)))]
+    `(lambda ,lambda-args ,(random-L5 d lambda-args))))
+  
   (let ([n (if (= depth 0)
                0 (random 6))]
         [d (- depth 1)])
@@ -16,12 +29,13 @@
                             ,(random-L5 d vars))]
       [(2) `(begin ,(random-L5 d vars)
                    ,(random-L5 d vars))]
-      [(3) (let ([var-name (rand-sym)])
+      [(3) (let ([var-name (rand-varname)])
              `(let ([,var-name ,(random-L5 d vars)])
                 ,(random-L5 d (cons var-name vars))))]
-      [else (random-L5 depth vars)])))
+      [(4) (random-lambda d vars)]
+      [else (random-L5 d vars)])))
 
-(define (rand-sym)
+(define (rand-varname)
     (rand-elm-from '(var1 var2 var3 var4)))
 
 (define (rand-elm-from lst)
@@ -38,7 +52,7 @@
                    )))
 
 (pretty-write
- (gen-random-L5 8))
+ (gen-random-L5 5))
 
 
 
