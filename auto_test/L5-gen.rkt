a7252f9ffc04ea737a61044ebc154318cda391ad
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-05-23 15:40:24 -0500

    add application expressions and make things work mostly

--- auto_test/L5-gen.rkt
+++ auto_test/L5-gen.rkt
@@ -1,7 +1,6 @@
 #lang plai
 
-(define (gen-random-L5 depth)
-  (random-L5 depth '()))
+
 
 ; current issue.  How do I stop procedures from being added to numbers?
 ; need a specific type for L5-vals? numbers and procedures? That's a pain
@@ -13,8 +12,43 @@
 ; the other places (bodys of lets) I can put anything
 ; and in function application, I can only put lambdas
 
+#|
+e ::= (lambda (x ...) e)
+    | x
+    | (let ([x e]) e)
+    | (letrec ([x e]) e)
+    | (if e e e)
+    | (new-tuple e ...)
+    | (begin e e)
+    | (e e ...) ;; application expression
+    | prim
+    | num
 
-(define arg-vars '(arg1 arg2 arg3 arg4 arg4))
+| (lambda (x ...) any-e)
+| x
+| (let ([x any-e]) any-e)
+| (letrec ([x any-e]) any-e)
+| (if num-e any-e any-e)
+| (new-tuple any-e ...)
+| (begin any-e any-e)
+| (proc-e any-e ...)
+| (biop num-e)
+| (pred any-e)
+| (print num-e/arr-e)
+| (new-array num-e any-e)
+| (aref arr-e num-e)
+| (aset arr-e num-e any-e)
+| (alen arr-e)
+
+biop ::= + | - | * | < | <= | =
+pred ::= number? | a?
+
+
+|#
+(define (gen-random-L5 depth)
+  `(print ,(random-L5 depth '())))
+
+(define param-vars '(par1 par2 par3 par4 par4))
 (define var-list '(v1 v2 v3 v4 v5 v6 v7))
 (define proc-vars '(f1 f2 f3 f4 f5 f6 f7))
 (define array-vars '(a1 a2 a3 a4 a5 a6 a7))
@@ -24,26 +58,15 @@
        (procs (listof symbol?))
        (arrays (listof symbol?))])
 
-(define/contract (random-L5 depth vars)
-  
-  (define (random-lambda d bindings)
-    (define random-arglist
-      (λ (len)
-        (if (= len 0)
-            '()
-            (cons (list-ref arg-vars len)
-                  (random-arglist (- len 1))))))
-    (let [(lambda-args (random-arglist (random 4)))]
-    `(lambda ,lambda-args ,(random-L5 d lambda-args))))
-  
+(define (random-L5 depth vars) 
   (let ([n (if (= depth 0)
-               0 (random 6))]
+               0
+               (+ 1 (random 6)))]
         [d (- depth 1)])
     (case n
-      [(0) (if (= depth 0)
-               `(print ,(rand-elm-from (cons (random 20)
-                                             vars)))
-               `(print ,(random 20)))]
+      [(0)  (if (empty? vars)
+                (random 20)
+                (rand-elm-from vars))]
       [(1) `(,(rand-biop) ,(random-L5 d vars)
                           ,(random-L5 d vars))]
       [(2) `(begin ,(random-L5 d vars)
@@ -51,8 +74,21 @@
       [(3) (let ([var-name (rand-varname)])
              `(let ([,var-name ,(random-L5 d vars)])
                 ,(random-L5 d (cons var-name vars))))]
-      [(4) (random-lambda d vars)]
+      [(4) (let* ([arity (random 5)]
+                  [params (take param-vars arity)]
+                  [lam (random-lambda params d vars)]
+                  [args (list-of-n-L5s arity d vars)])
+             `(,lam ,@args))]
+      [(5) `(,(rand-elm-from '(number? a?)) (random-L5 d vars))]
       [else (random-L5 depth vars)])))
+
+(define (random-lambda params depth binds)
+  `(lambda ,params ,(random-L5 depth (append params binds))))
+
+(define (list-of-n-L5s n depth vars)
+  (for/list [(i (in-range n))]
+    (random-L5 depth vars)))
+
 
 (define (rand-varname)
     (rand-elm-from var-list))
@@ -71,7 +107,7 @@
                    )))
 
 (pretty-write
- (gen-random-L5 5))
+ (gen-random-L5 4))
 
 
 
--- auto_test/test-valid-L5.sh
+++ auto_test/test-valid-L5.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 racket L5-gen.rkt > test_file.L5
+echo "testing L5 expression: "
+cat test_file.L5
+
 ../322-interps/L5 test_file.L5
