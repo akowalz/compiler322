1d7e09e84e6b48f85880f498cdac87a03ab6eee2
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-05-23 16:02:36 -0500

    refactor

--- auto_test/L5-gen.rkt
+++ auto_test/L5-gen.rkt
@@ -1,17 +1,5 @@
 #lang plai
 
-
-
-; current issue.  How do I stop procedures from being added to numbers?
-; need a specific type for L5-vals? numbers and procedures? That's a pain
-; Could perhaps I just have two functions?  One that makes number expers
-; and other that makes lambda exprs
-; well, I basically already have that
-; I just need to replace all the places where ONLY number can go with
-; (rand-num-exr depth bindings..)
-; the other places (bodys of lets) I can put anything
-; and in function application, I can only put lambdas
-
 #|
 e ::= (lambda (x ...) e)
     | x
@@ -43,44 +31,64 @@ e ::= (lambda (x ...) e)
 biop ::= + | - | * | < | <= | =
 pred ::= number? | a?
 
-
 |#
+
 (define (gen-random-L5 depth)
-  `(print ,(random-L5 depth '())))
+  `(print ,(random-L5 depth '()))) 
 
 (define param-vars '(par1 par2 par3 par4 par4))
 (define var-list '(v1 v2 v3 v4 v5 v6 v7))
 (define proc-vars '(f1 f2 f3 f4 f5 f6 f7))
 (define array-vars '(a1 a2 a3 a4 a5 a6 a7))
+(define biops '(+ - * < <= =))
 
 (define-type environment
   [env (nvars (listof symbol?))
        (procs (listof symbol?))
        (arrays (listof symbol?))])
 
-(define (random-L5 depth vars) 
+(define (random-L5 depth vars)
+  
   (let ([n (if (= depth 0)
                0
-               (+ 1 (random 6)))]
+               (+ 1 (random 8)))]
         [d (- depth 1)])
+    
+    (define (rand-biop-expr)
+      `(,(rand-biop) ,(random-L5 d vars)
+                     ,(random-L5 d vars)))
+    (define (rand-value)
+      (if (empty? vars)
+          (random 20)
+          (rand-elm-from vars)))
+    (define (rand-begin-expr)
+      `(begin ,(random-L5 d vars)
+              ,(random-L5 d vars)))
+    (define (rand-let-expr)
+      (let ([var-name (rand-varname)])
+        `(let ([,var-name ,(random-L5 d vars)])
+           ,(random-L5 d (cons var-name vars)))))
+    (define (rand-app-expr)
+      (let* ([arity (random 5)]
+             [params (take param-vars arity)]
+             [lam (random-lambda params d vars)]
+             [args (list-of-n-L5s arity d vars)])
+        `(,lam ,@args)))
+    (define (rand-pred-expr)
+      `(,(rand-elm-from '(number? a?)) ,(random-L5 d vars)))
+    (define (rand-if-expr)
+      `(if ,@(list-of-n-L5s 3 d vars)))
+    
     (case n
-      [(0)  (if (empty? vars)
-                (random 20)
-                (rand-elm-from vars))]
-      [(1) `(,(rand-biop) ,(random-L5 d vars)
-                          ,(random-L5 d vars))]
-      [(2) `(begin ,(random-L5 d vars)
-                   ,(random-L5 d vars))]
-      [(3) (let ([var-name (rand-varname)])
-             `(let ([,var-name ,(random-L5 d vars)])
-                ,(random-L5 d (cons var-name vars))))]
-      [(4) (let* ([arity (random 5)]
-                  [params (take param-vars arity)]
-                  [lam (random-lambda params d vars)]
-                  [args (list-of-n-L5s arity d vars)])
-             `(,lam ,@args))]
-      [(5) `(,(rand-elm-from '(number? a?)) (random-L5 d vars))]
+      [(0) (rand-value)]
+      [(1) (rand-biop-expr)]
+      [(2) (rand-begin-expr)]
+      [(3) (rand-let-expr)]
+      [(4) (rand-app-expr)]
+      [(5) (rand-pred-expr)]
+      [(6) (rand-if-expr)]
       [else (random-L5 depth vars)])))
+
 
 (define (random-lambda params depth binds)
   `(lambda ,params ,(random-L5 depth (append params binds))))
@@ -88,7 +96,6 @@ pred ::= number? | a?
 (define (list-of-n-L5s n depth vars)
   (for/list [(i (in-range n))]
     (random-L5 depth vars)))
-
 
 (define (rand-varname)
     (rand-elm-from var-list))
@@ -98,16 +105,10 @@ pred ::= number? | a?
     (list-ref lst n)))
 
 (define (rand-biop)
-  (rand-elm-from '(+
-                   -
-                   *
-                   <
-                   <=
-                   =
-                   )))
+  (rand-elm-from biops))
 
 (pretty-write
- (gen-random-L5 4))
+ (gen-random-L5 2))
 
 
 
